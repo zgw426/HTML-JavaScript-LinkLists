4e1efdc5f3faf6043963da884628d7d7bd4a7d68
suo <zgw426@gmail.com>
2021-11-07 16:28:31 +0900

    update

--- links_02.json.js
+++ links_02.json.js
@@ -49,6 +49,20 @@ let list02 = [
         { "key": "ニュース", "kana": "にゅーす"}
       ],
     "note"     : "メモ"
-  }
+  },
+
+  {
+    "kana"     : "クーグル検索",
+    "Name"     : "Google検索(拡張現実)",
+    "URL"      : "https://www.google.com/search?q=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&rlz=1C1CHBD_jaJP878JP878&oq=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&aqs=chrome..69i57j0i512l9.3010j1j15&sourceid=chrome&ie=UTF-8",
+    "category" : 
+      [
+        { "key": "検索", "kana": "けんさく"}
+      ],
+    "note"     : "検索キー:拡張現実"
+  },
+
+
+
 ];
 
