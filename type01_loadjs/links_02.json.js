def08138e794903741f19891d22c6a5875c64280
takehumi suou <zgw426@gmail.com>
2022-12-11 00:50:33 +0900

    カテゴリキーにアイコンを表示する機能を追加

--- type01_loadjs/links_02.json.js
+++ type01_loadjs/links_02.json.js
@@ -1,100 +1,84 @@
 let list02 = {
 "info": {"Title":"サイト","ID":"02"},
 "list": [
-  {
-    "aicon"    : "amazon.png",
-    "kana"     : "アマゾン",
-    "Name"     : "Amazon",
-    "URL"      : "https://www.amazon.co.jp/",
-    "category" : 
-      [
-        { "key": "買い物", "kana": "かいもの"}
-      ],
-      "note"     : "Amazon.com, Inc.は、ワシントン州シアトルに本拠地を置くアメリカの多国籍テクノロジー企業らしい by Wikipedia\n\nログインID：hoge\nログインパスワード：hogefuga?123"
-  },
-
-  {
-    "kana"     : "らくてんいちば",
-    "Name"     : "楽天市場",
-    "URL"      : "https://www.rakuten.co.jp/",
-    "category" : 
-      [
-        { "key": "買い物", "kana": "かいもの"},
-        { "key": "楽天", "kana": "らくてん"}
-      ],
-      "note"     : "楽天"
-  },
-
-  {
-    "kana"     : "キャオ",
-    "Name"     : "Gyao!",
-    "URL"      : "https://gyao.yahoo.co.jp/",
-    "category" : 
-      [
-        { "key": "動画", "kana": "とうが"}
-      ],
-      "note"     : "メモ"
-  },
-  {
-    "aicon"    : "youtube.png",
-    "kana"     : "ユーチューブ",
-    "Name"     : "YouTube",
-    "URL"      : "https://www.youtube.com/",
-    "category" : 
-      [
-        { "key": "動画", "kana": "とうが"},
-        { "key": "YouTube", "kana": "ユーチューブ"}
-      ],
-    "note"     : "メモ"
-  },
-  {
-    "kana"     : "ツイッター",
-    "Name"     : "Twitter",
-    "URL"      : "https://twitter.com/",
-    "category" : 
-      [
-        { "key": "SNS", "kana": "エスエヌエス"}
-      ],
-    "note"     : "メモ"
-  },
-  {
-    "kana"     : "コードジン",
-    "Name"     : "CodeZine（コードジン）",
-    "URL"      : "https://codezine.jp/",
-    "category" : 
-      [
-        { "key": "ニュース", "kana": "にゅーす"}
-      ],
-    "note"     : "メモ"
-  },
-
-  {
-    "aicon"    : "google.png",
-    "kana"     : "クーグルけんさくかくちょうげんじつ",
-    "Name"     : "Google検索(拡張現実)",
-    "URL"      : "https://www.google.com/search?q=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&rlz=1C1CHBD_jaJP878JP878&oq=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&aqs=chrome..69i57j0i512l9.3010j1j15&sourceid=chrome&ie=UTF-8",
-    "category" : 
-      [
-        { "key": "検索", "kana": "けんさく"}
-      ],
-    "note"     : "検索キー:拡張現実"
-  },
-
-  {
-    "aicon"    : "google.png",
-    "kana"     : "クーグルけんさくエーアイ",
-    "Name"     : "Google検索(AI)",
-    "URL"      : "https://www.google.com/search?q=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&rlz=1C1CHBD_jaJP878JP878&oq=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&aqs=chrome..69i57j0i512l9.3010j1j15&sourceid=chrome&ie=UTF-8",
-    "category" : 
-      [
-        { "key": "検索", "kana": "けんさく"}
-      ],
-    "note"     : "検索キー:AI"
-  },
-
-
-
-
-
-]
-};
+{
+	"aicon": "amazon.png",
+	"kana": "アマゾン",
+	"Name": "Amazon",
+	"URL": "https://www.amazon.co.jp/",
+	"category":
+		[
+			{ "key": "買い物", "kana": "かいもの"}
+		],
+	"note": "Amazon.com, Inc.は、ワシントン州シアトルに本拠地を置くアメリカの多国籍テクノロジー企業らしい by Wikipedia\n\nログインID：hoge\nログインパスワード：hogefuga?123"
+},{
+	"kana": "らくてんいちば",
+	"Name": "楽天市場",
+	"URL": "https://www.rakuten.co.jp/",
+	"category":
+		[
+			{ "key": "買い物", "kana": "かいもの"},
+			{ "key": "楽天", "kana": "らくてん"}
+		],
+	"note": "楽天"
+},{
+	"kana": "キャオ",
+	"Name": "Gyao!",
+	"URL": "https://gyao.yahoo.co.jp/",
+	"category":
+		[
+			{ "key": "動画", "kana": "とうが"}
+		],
+	"note": "メモ"
+},{
+	"aicon": "youtube.png",
+	"kana": "ユーチューブ",
+	"Name": "YouTube",
+	"URL": "https://www.youtube.com/",
+	"category":
+		[
+			{ "key": "動画", "kana": "とうが"},
+			{ "key": "YouTube", "kana": "ユーチューブ"}
+		],
+	"note": "メモ"
+},{
+	"kana": "ツイッター",
+	"Name": "Twitter",
+	"URL": "https://twitter.com/",
+	"category":
+		[
+			{ "key": "SNS", "kana": "エスエヌエス"}
+		],
+	"note": "メモ"
+},{
+	"kana": "コードジン",
+	"Name": "CodeZine（コードジン）",
+	"URL": "https://codezine.jp/",
+	"category":
+		[
+			{ "key": "ニュース", "kana": "にゅーす"}
+		],
+	"note": "メモ"
+},{
+	"aicon": "google.png",
+	"kana": "クーグルけんさくかくちょうげんじつ",
+	"Name": "Google検索(拡張現実)",
+	"URL": "https://www.google.com/search?q=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&amp;rlz=1C1CHBD_jaJP878JP878&amp;oq=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&amp;aqs=chrome..69i57j0i512l9.3010j1j15&amp;sourceid=chrome&amp;ie=UTF-8",
+	"category":
+		[
+			{ "key": "検索", "kana": "けんさく"}
+		],
+	"note": "検索キー:拡張現実"
+},{
+	"aicon": "google.png",
+	"kana": "クーグルけんさくエーアイ",
+	"Name": "Google検索(AI)",
+	"URL": "https://www.google.com/search?q=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&amp;rlz=1C1CHBD_jaJP878JP878&amp;oq=%E6%8B%A1%E5%BC%B5%E7%8F%BE%E5%AE%9F&amp;aqs=chrome..69i57j0i512l9.3010j1j15&amp;sourceid=chrome&amp;ie=UTF-8",
+	"category":
+		[
+			{ "key": "検索", "kana": "けんさく"},
+			{ "key": "AI", "kana": "エーアイ"}
+		],
+	"note": "検索キー:AI"
+},
+]};
--- type01_loadjs/links_04.json.js
+++ type01_loadjs/links_04.json.js
@@ -1,15 +1,51 @@
 let list04 = {
-  "info": {"Title":"links_04リスト","ID":"04"},
-  "list": [
-  {
-    "kana"     : "ゼロヨン",
-    "Name"     : "04",
-    "URL"      : "",
-    "category" : 
-      [
-        { "key": "04", "kana": "ゼロヨン"}
-      ],
-    "note"     : ""
-  },
-]
-};
+"info": {"Title":"統計学","ID":"04"},
+"list": [
+{
+	"aicon": "study.png",
+	"kana": "とうけいすうりけんきゅうしょのこうぎどうが",
+	"Name": "統計数理研究所の講義動画",
+	"URL": "https://youtube.com/channel/UCjfiNEuZBwxaG_-F_Bx0BGA/playlists",
+	"category":
+		[
+			{ "key": "統計", "kana": "とうけい"},
+			{ "key": "勉強", "kana": "へんきょう"},
+			{ "key": "動画", "kana": "とうが"}
+		],
+	"note": ""
+},{
+	"aicon": "study.png",
+	"kana": "すうがくカフェそくしゅうかくりつとうけい",
+	"Name": "数学カフェ「速習 確率・統計」",
+	"URL": "https://slideshare.net/matsukenbook/rev012",
+	"category":
+		[
+			{ "key": "統計", "kana": "とうけい"},
+			{ "key": "勉強", "kana": "へんきょう"}
+		],
+	"note": ""
+},{
+	"aicon": "study.png",
+	"kana": "でーたぶんせきのためのとうけいがくにゅうもん",
+	"Name": "データ分析のための統計学入門 第4版 pdf",
+	"URL": "http://kunitomo-lab.sakura.ne.jp/2021-3-3Open(S).pdf",
+	"category":
+		[
+			{ "key": "統計", "kana": "とうけい"},
+			{ "key": "勉強", "kana": "へんきょう"}
+		],
+	"note": ""
+},{
+	"aicon": "study.png",
+	"kana": "きょうだいのこうぎどうが",
+	"Name": "京大の講義動画「大学生のための統計学入門」",
+	"URL": "https://ocw.kyoto-u.ac.jp/course/809/",
+	"category":
+		[
+			{ "key": "統計", "kana": "とうけい"},
+			{ "key": "勉強", "kana": "へんきょう"},
+			{ "key": "動画", "kana": "とうが"}
+		],
+	"note": ""
+},
+]};
