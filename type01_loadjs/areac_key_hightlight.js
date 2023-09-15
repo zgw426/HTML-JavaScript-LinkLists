7bad9200054ef2e81ec870a9dda0f80f9eaf598c
takehumi suou <zgw426@gmail.com>
2023-09-15 13:50:08 +0900

    カテゴリーキーの強調表示機能を追加@

---
+++ type01_loadjs/areac_key_hightlight.js
@@ -0,0 +1,7 @@
+// 特定のカテゴリキーを強調表示して目立たせる
+let key_hightlight = new Array();
+key_hightlight.length = 0;
+
+// ----- category key aicon ----- //
+key_hightlight["ニュース"] = "#ffc0cb";
+key_hightlight["マイクロソフト"] = "#c0ffcb";
